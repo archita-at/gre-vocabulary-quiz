e83830093125f1ca9a15c0c7c32d1e51aeb4cfdf
Archita Totla <architatotla@gmail.com>
2021-07-13 19:04:02 +0530

    created add-word form

---
+++ quiz-frontend/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AddWordForm from './components/addWordForm';
+
+ReactDOM.render(<AddWordForm />, document.getElementById('root'));

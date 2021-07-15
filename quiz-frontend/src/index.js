6eec561d615f9339c0b6a6e03c89fedd16fd9a30
Archita Totla <architatotla@gmail.com>
2021-07-15 12:09:04 +0530

    deleted quiz-frontend

--- quiz-frontend/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AddWordForm from './components/addWordForm';
-
-ReactDOM.render(<AddWordForm />, document.getElementById('root'));
